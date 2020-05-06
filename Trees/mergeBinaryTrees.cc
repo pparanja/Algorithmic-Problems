2be92199999740f76baa68c4f03babc71fdc7336
prathpar <64886321+prathpar@users.noreply.github.com>
2020-05-06 20:12:32 +0530

    Update mergeBinaryTrees.cc

--- Trees/mergeBinaryTrees.cc
+++ Trees/mergeBinaryTrees.cc
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Method - 1 : Creating new tree 
+
 class Solution {
 private:
     void processInOrder(TreeNode* t1, TreeNode* t2, TreeNode* t) {
@@ -36,5 +38,27 @@ public:
         }
         processInOrder(t1, t2, t);
         return t;
+    }
+};
+
+//==============================================================================================
+//Method - 2 : Modifying same tree 
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        // Modify one of the existing tree to hold the output
+        if (!t1) {
+            return t2;
+        }
+        
+        if (!t2) {
+            return t1;
+        }
+        
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        
+        return t1;    
     }
 };
